e5441db0435c5b45b8ea2cb27dd6e0eac257718e
Aman Yadav <kr.amanyadav275@gmail.comm>
2026-09-14 01:44:47 +0530

    Update API URL for deployment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useMemo, useState } from "react";
 import axios from "axios";
 
-const API = "http://localhost:5000/api";
+const API = import.meta.env.VITE_API_URL;
 
 function App() {
   const [products, setProducts] = useState([]);
